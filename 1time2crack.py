49d508e760491121106db7240dbb99bdd2d0222d
Ryan B Hayward <hayward@ualberta.ca>
2016-02-04 14:37:44 -0700

    helper tool for twice-used 1tpad cracking

---
+++ 1time2crack.py
@@ -0,0 +1,42 @@
+import sys
+from string import ascii_lowercase
+
+L = sys.stdin.readlines()
+  
+for k in range(len(L)):
+  L[k] = L[k].strip('\n')
+
+def alphaOp(s1,op,s2):
+  mylen = min(len(s1),len(s2))
+  result = ''
+  assert op in '+-'
+  for j in range(mylen):
+    c1,c2 = s1[j],s2[j]
+    if c1 in ascii_lowercase and c2 in ascii_lowercase:
+      if op == '-': 
+        offset = ( ord(c1) - ord(c2) ) %26
+      else:
+        offset = ( ord(c1) + ord(c2) - 2*ord('a')) %26
+      result += chr(ord('a') + offset)
+    elif c1==' ' and c2==' ':
+      result += ' '
+    else:
+      result += '.'
+  return result
+
+indent = '  '
+for k in L:
+  print indent + k
+  
+for j in range(2):
+  print '\ntry guess as plaintext', 1+j
+  key =  alphaOp(L[j],'-',L[2])
+  print indent + key
+  print indent + alphaOp(L[1-j],'-',key)
+
+#for k in range(len(L)):
+  #for j in range(len(L[k])):
+    #if L[k][j] != ' ':
+      #print chr(ord('a') + (ord(L[0][j]) - ord(L[k][j]))%26),
+  #print ''
+
---
+++ space.py
@@ -0,0 +1,8 @@
+import sys
+
+L = sys.stdin.readlines()
+  
+for line in L:
+  for c in line:
+    print c,
+  print ''
